eb469f5433b9b249a23d628c9bc1967f96efddb0
Sebastian Ospina <juansebastian200103@gmail.com>
2022-02-08 21:45:41 -0500

    Intento de solucion

--- src/main/java/edu/escuelaing/arep/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/SparkWebApp.java
@@ -14,29 +14,29 @@ import edu.escuelaing.Categorias.temperatura.*;
 public class SparkWebApp {
 
     public static void main(String[] args) {
+        staticFiles.location("/public");
         port(getPort());
-        staticFileLocation("/html");
         init();
         after((Filter) (request, response) -> {
             response.header("Access-Control-Allow-Origin", "*");
             response.header("Access-Control-Allow-Methods", "GET");
         });
         path("/Temperatura",()->{
-            get("/Celsius", (req, res)->{
+            get("/Celsius",  "application/json",(req, res)->{
                 return getCelsius(Double.valueOf(req.queryParams("value")));
             });
-            get("/Fahrenheit", (req, res)->{
+            get("/Fahrenheit",  "application/json",(req, res)->{
                 return getFahrenheit(Double.valueOf(req.queryParams("value")));
             });
         });  
     }
 
-    private static JsonElement getCelsius(double valor) {
-        return new Gson().toJsonTree(new Celsius(valor));
+    private static String getCelsius(double valor) {
+        return new Gson().toJson(new Celsius(valor));
     }
 
-    private static JsonElement getFahrenheit(double valor) {
-        return new Gson().toJsonTree(new Fahrenheit(valor));
+    private static String getFahrenheit(double valor) {
+        return new Gson().toJson(new Fahrenheit(valor));
     }
 
     static int getPort() {
